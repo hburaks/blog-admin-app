da174a59239dafd02d0fc2eba2329403ed4da88f
Hasan Burak Songur <hasanburaksongur@gmail.com>
2023-07-01 20:47:51 +0300

    add next and prev page functions

--- src/app/layout/content/table/table.component.ts
+++ src/app/layout/content/table/table.component.ts
@@ -9,6 +9,9 @@ export class TableComponent {
   @Input() table!: any[];
   isAddNewItem : boolean = false;
   isEditItem : boolean[] = [];
+  pageIndex : number = 0;
+  pageSize : number = 10;
+  linkId : number = this.pageIndex + 1;
 
   constructor(
     private tableService : TableService,
@@ -28,7 +31,22 @@ export class TableComponent {
   showEditItemCard(i : number){
     this.isEditItem[i] = !this.isEditItem[i];
   }
-  
+  nextPage(){
+    let totalPage = this.table.length / this.pageSize;
+    let remainingItems = this.table.length % this.pageSize;
+    if(remainingItems > 0){
+      totalPage++
+    }
+    if(this.pageIndex + 1 <= totalPage - 1 ){
+      this.pageIndex = this.pageIndex + 1;
+    }
+  }
+  prevPage(){
+    
+    if(this.pageIndex - 1 > 0 ){
+      this.pageIndex = this.pageIndex - 1;
+    }
+  }
 }
 
 
