803ce81f0dccc44c18845c6dd7efddbbb1eb2485
Hasan Burak Songur <hasanburaksongur@gmail.com>
2023-07-12 17:29:54 +0300

    Adding style

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
+  isSideBar : boolean = false;
+  isSideBarSubscription : Subscription | undefined;
+
   navigateToGitHub() {
     window.open("https://github.com/hburaks/blog-admin-app", "_blank");
   }
+
+  toggleIsSideBar(){
+    this.isSideBar = !this.isSideBar;
+  }
+  ngOnInit(): void {
+    
+  }
+  
 }
